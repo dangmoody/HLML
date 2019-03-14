239dac06f49acc737fb43270e45210f889146a04
Dan Moody <daniel.guy.moody@gmail.com>
2019-03-14 21:23:19 +0000

    forgot a file

--- generator/gen_doc_common.h
+++ generator/gen_doc_common.h
@@ -2,33 +2,16 @@
 
 #include <string>
 
-inline std::string	Gen_GetDocCtorDefault( void );
-inline std::string	Gen_GetDocCtorCopy( void );
-
 inline std::string	Gen_GetDocOperatorEquals( const std::string& fullTypeName );
 inline std::string	Gen_GetDocOperatorNotEquals( const std::string& fullTypeName );
 
 
-std::string Gen_GetDocCtorDefault( void ) {
-	return "\t/// Default constructor.  Initializes all values to zero.\n";
-}
-
-std::string Gen_GetDocCtorCopy( void ) {
-	return "\t/// Copy constructor.  Copies the elements of the given vector via memcpy.\n";
-}
-
 std::string Gen_GetDocOperatorEquals( const std::string& fullTypeName ) {
-	std::string doc;
-	doc += "/// \\relates " + fullTypeName + "\n";
-	doc += "/// \\brief Returns true if the all the components of the left-hand-side " + fullTypeName + " match the other one, otherwise returns false.\n";
-
-	return doc;
+	return "/// \\relates " + fullTypeName + "\n" \
+		"/// \\brief Returns true if the all the components of the left-hand-side " + fullTypeName + " match the other one, otherwise returns false.\n";
 }
 
 std::string Gen_GetDocOperatorNotEquals( const std::string& fullTypeName ) {
-	std::string doc;
-	doc += "/// \\relates " + fullTypeName + "\n";
-	doc += "/// \\brief Returns true if not all of the components of the left-hand-side " + fullTypeName + " match the other one, otherwise returns false.\n";
-
-	return doc;
+	return "/// \\relates " + fullTypeName + "\n" \
+		"/// \\brief Returns true if not all of the components of the left-hand-side " + fullTypeName + " match the other one, otherwise returns false.\n";
 }
