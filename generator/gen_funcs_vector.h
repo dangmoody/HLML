4e236be2d8d757886325a61fffaf8d200e693c11
Dan Moody <daniel.guy.moody@gmail.com>
2019-03-14 20:01:26 +0000

    remove cruft

--- generator/gen_funcs_vector.h
+++ generator/gen_funcs_vector.h
@@ -4,9 +4,6 @@
 
 extern std::string	Gen_GetParmListVector( const genType_t type, const uint32_t numComponents, const float* values );
 
-//extern void	Gen_VectorOperatorsArithmetic( const genType_t type, const uint32_t numComponents, std::string& outHeader, std::string& outInl );
-//extern void	Gen_VectorOperatorsRelational( const genType_t type, const uint32_t numComponents, std::string& outHeader, std::string& outInl );
-
 extern void			Gen_VectorLength( const genType_t type, const uint32_t numComponents, std::string& outHeader, std::string& outInl );
 extern void			Gen_VectorNormalize( const genType_t type, const uint32_t numComponents, std::string& outHeader, std::string& outInl );
 extern void			Gen_VectorDot( const genType_t type, const uint32_t numComponents, std::string& outHeader, std::string& outInl );
