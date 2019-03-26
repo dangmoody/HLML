24889a4e3276df467ffabc850769c8906802fc85
Dan Moody <daniel.guy.moody@gmail.com>
2019-03-26 00:29:27 +0000

    update build script for vs code

--- build.bat
+++ build.bat
@@ -1,9 +1,11 @@
 @echo off
 
-set config=%1
+set out_name=%1
+set config=%2
+set source_file_root=%3
 
 set warning_flags=-Wall -Wextra -Werror
 set ignore_warnings=-Wno-c++98-compat-pedantic -Wno-covered-switch-default -Wno-newline-eof -Wno-reserved-id-macro -Wno-global-constructors -Wno-exit-time-destructors
 
 echo Building...
-clang++ -Xclang -flto-visibility-public-std -std=c++14 -g -o %config%\hlml-gen_%config%.exe generator\*.cpp %warning_flags% %ignore_warnings%
+clang++ -Xclang -flto-visibility-public-std -std=c++14 -g -o build\%config%\%out_name%-%config%.exe %source_file_root%\*.cpp %warning_flags% %ignore_warnings%
