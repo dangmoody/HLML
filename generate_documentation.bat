f55155ea2b5c32a36b1e788f1a24322e24480716
Dan Moody <daniel.guy.moody@gmail.com>
2019-03-31 00:26:04 +0000

    generate symlink to documentation root and ignore it locally

--- generate_documentation.bat
+++ generate_documentation.bat
@@ -1,3 +1,7 @@
 @echo off
 
+REM call doxygen to generate documentation
 "doxygen/doxygen" doxygen/config
+
+REM generate a shortcut to the documentation index.html
+mklink html_docs.html documentation\html\index.html
