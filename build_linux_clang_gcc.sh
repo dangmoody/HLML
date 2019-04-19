904e38faa16a62e696f05b2054281e0dbde7547f
Dan Moody <daniel.guy.moody@gmail.com>
2019-04-19 16:35:34 +0100

    add release optimisation options, fix gcc options

--- build_linux_clang_gcc.sh
+++ build_linux_clang_gcc.sh
@@ -3,15 +3,39 @@
 # exit on first failure
 set -e
 
-compiler=$1
-config=$2
+compiler=$1	# can be either "clang++" or "g++"
+config=$2	# can be either "debug" or "release"
 
 source_files=code/generator/*.cpp
 
-# TODO(DM): separate compile options and ignore warnings between clang and gcc
 # TODO(DM): add release compiler options (-O3, -ffast-math, etc.)
-options_compiler="-Xclang -flto-visibility-public-std"
-options_error="-Wall -Wextra -Weverything -Wpedantic"
-ignore_warnings="-Wno-c++98-compat-pedantic -Wno-covered-switch-default -Wno-newline-eof -Wno-reserved-id-macro -Wno-global-constructors -Wno-exit-time-destructors -Wno-padded"
 
-$compiler $options_compiler -o build/$config/hlml-gen.exe $source_files $options_error $ignore_warnings
+if [[ $compiler == clang* ]]; then
+	options_std="-Xclang -flto-visibility-public-std"
+else
+	options_std=""
+fi
+
+options_compiler=${options_std}
+
+if [[ $config = release ]]; then
+	options_compiler="${options_compiler} -O3 -ffast-math"
+fi
+
+options_error="-Wall -Wextra -Wpedantic"
+if [[ $compiler == clang* ]]; then
+	options_error="${options_error} -Weverything"
+fi
+
+ignore_warnings="-Wno-c++98-compat-pedantic -Wno-covered-switch-default -Wno-newline-eof -Wno-reserved-id-macro -Wno-global-constructors -Wno-exit-time-destructors -Wno-padded -Wno-unused-macros"
+
+echo ------- Building for $compiler -------
+
+echo Building with options : ${options_compiler}
+echo Enabled errors        : ${options_error}
+echo Ignoring options      : ${ignore_warnings}
+echo ""
+
+${compiler} ${options_compiler} -o build/${config}/hlml-gen.exe ${source_files} ${options_error} ${ignore_warnings}
+
+echo ------- Done. -------
