2081ad2487dec812b7be009cef4f527a025596d1
Dan Moody <daniel.guy.moody@gmail.com>
2019-04-19 01:24:51 +0100

    add vs code project files again

--- build_linux_clang_gcc.sh
+++ build_linux_clang_gcc.sh
@@ -4,11 +4,14 @@
 set -e
 
 compiler=$1
+config=$2
 
 source_files=code/generator/*.cpp
 
 # TODO(DM): separate compile options and ignore warnings between clang and gcc
+# TODO(DM): add release compiler options (-O3, -ffast-math, etc.)
+options_compiler="-Xclang -flto-visibility-public-std"
 options_error="-Wall -Wextra -Weverything -Wpedantic"
 ignore_warnings="-Wno-c++98-compat-pedantic -Wno-covered-switch-default -Wno-newline-eof -Wno-reserved-id-macro -Wno-global-constructors -Wno-exit-time-destructors -Wno-padded"
 
-$compiler -o hlml-gen.exe $source_files $options_error $ignore_warnings
+$compiler $options_compiler -o build/$config/hlml-gen.exe $source_files $options_error $ignore_warnings
