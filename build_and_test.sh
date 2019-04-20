c6d31c0e28c3dceac6200959b02a720f27b8514c
Dan Moody <daniel.guy.moody@gmail.com>
2019-04-20 22:09:12 +0100

    make travis build and run generator and tests

---
+++ build_and_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# exit on first failure
+set -e
+
+compiler=$1	# can be either "clang++" or "gcc"
+config=$2	# can be either "debug" or "release"
+
+echo ------- Building generator -------
+source_files_path="code/generator"
+source build_linux_clang_gcc.sh ${compiler} ${config} hlml-gen.exe ${source_files_path}
+echo ------- Done -------
+echo ""
+
+echo ------- Running generator -------
+build/${config}/hlml-gen.exe
+echo ------- Done -------
+echo ""
+
+echo ------- Building tests -------
+source_files_path="code/tests"
+source build_linux_clang_gcc.sh ${compiler} ${config} hlml-gen-tests.exe ${source_files_path}
+echo ------- Done -------
+echo ""
+
+echo ------- Running tests -------
+build/${config}/hlml-gen-tests.exe -c
+echo ------- Done -------
+echo ""
