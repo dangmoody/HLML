ae324634763c0e2ebfcf01977fe58407c62481a9
Dan Moody <daniel.guy.moody@gmail.com>
2019-05-25 17:54:00 +0100

    fix hardcoded path to cl.exe (which obviously isn't going to work)

--- build_and_test_msvc.bat
+++ build_and_test_msvc.bat
@@ -1,15 +1,16 @@
 @echo off
 
+REM can be "debug" or "release"
 set config=%1
 
-set msvc_path="C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.16.27023\\bin\\Hostx64\\x64\\cl.exe"
+REM path to vcvars64.bat, MUST include file name
+set vcvars64_path=%3
 
-rem set vcvars64_path="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-REM call %vcvars64_path%
+call %vcvars64_path%
 
 echo ------- Building generator -------
 set source_files=code\generator\*.cpp
-call build_msvc.bat %msvc_path% %config% hlml-gen %source_files%
+call build_msvc.bat %config% hlml-gen %source_files%
 echo ------- Done -------
 echo.
 
@@ -20,7 +21,7 @@ echo.
 
 echo ------- Building tests -------
 set source_files=code\tests\*.cpp
-call build_msvc.bat %msvc_path% %config% hlml-gen-tests %source_files%
+call build_msvc.bat %config% hlml-gen-tests %source_files%
 echo ------- Done -------
 echo.
 
--- build_msvc.bat
+++ build_msvc.bat
@@ -1,16 +1,13 @@
 @echo off
 
-REM path to CL.exe MUST include "CL.exe"
-set msvc_path=%1
-
 REM can be either "debug" or "release"
-set config=%2
+set config=%1
 
 REM name of the exe to make, must NOT include ".exe"
-set output_file=%3
+set output_file=%2
 
 REM path to source of files to build, must NOT include ".cpp"
-set source_files_path=%4
+set source_files_path=%3
 
 set options_compiler=/W4 /WX /MT /Od /MP /Gm- /EHsc /U "_UNICODE" /Fo"build\\msvc\\%config%\\intermediate\%output_file%\\"
 set options_linker=/OUT:build\\msvc\\%config%\\%output_file%.exe /NOLOGO /opt:ref
@@ -28,4 +25,4 @@ IF NOT EXIST "build\msvc\%config%\intermediate\%output_file%" (
 	mkdir "build\msvc\%config%\intermediate\%output_file%"
 )
 
-%msvc_path% %source_files_path% %additional_includes% %options_compiler% /link %options_linker% %libs%
+cl.exe %source_files_path% %additional_includes% %options_compiler% /link %options_linker% %libs%
