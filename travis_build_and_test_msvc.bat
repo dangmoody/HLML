83910afedb2521e16754b299371eee3fe2cd9b84
dan <daniel.guy.moody@gmail.com>
2020-08-23 23:46:11 +0100

    apparently 'msvc' is now a travis option

--- travis_build_and_test_msvc.bat
+++ travis_build_and_test_msvc.bat
@@ -9,7 +9,7 @@ if [%config%]==[] (
 	GOTO :bail
 )
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
 echo ------- Building generator -------
 set source_files=code\generator\*.cpp
