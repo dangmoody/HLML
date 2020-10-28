3b0f5efa863c20d82a67dd9fad655865d097c5fe
dan <daniel.guy.moody@gmail.com>
2020-10-28 19:41:53 +0000

    fix paths for make_release

--- scripts/make_release.bat
+++ scripts/make_release.bat
@@ -7,8 +7,6 @@ REM then build the tests
 
 setlocal EnableDelayedExpansion
 
-pushd %~dp0
-
 set version=%1
 
 if [%version%]==[] (
@@ -16,24 +14,27 @@ if [%version%]==[] (
 	goto :ShowUsage
 )
 
+pushd %~dp0
+pushd ..
+
 echo Packing build...
 
 REM copy the relevant files over into temp destinations
 REM these will be deleted later
-robocopy /s /e doxygen\ releases\doxygen\
 robocopy /s /e code\out\ releases\code\
 
-tools\\7zip\\7za.exe a -tzip releases\hlml_%version%.zip .\releases\code\ .\releases\doxygen\ generate_documentation.bat .\build\Release\hlml-gen-tests.exe
+tools\\7zip\\7za.exe a -tzip releases\hlml_%version%.zip .\releases\code\
 
 REM delete temp destinations
-rd /s /Q releases\doxygen\
 rd /s /Q releases\code\
 
 echo.
 echo Done.
 echo.
 
-pause
+popd
+popd
+
 goto :EOF
 
 
@@ -45,6 +46,3 @@ echo Arguments:
 echo     ^<version^> (required):
 echo         The version of the release that you are making (for example: 1.0.2 - where 1 is the major version, 0 is the minor version, and 2 is the patch version)
 goto :EOF
-
-
-popd
