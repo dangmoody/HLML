8526c1cd5c5a2628097f7124faacaf333d5b6dde
dan-linux <daniel.guy.moody@gmail.com>
2022-02-07 15:30:54 +0000

    add github actions support for linux

--- scripts/build_and_test_all.sh
+++ scripts/build_and_test_all.sh
@@ -3,9 +3,9 @@
 # exit when any command fails
 set -e
 
-source ./scripts/build_and_test_clang.sh
+source ./scripts/build_and_test_clang.sh clang
 
-source ./scripts/build_and_test_gcc.sh
+source ./scripts/build_and_test_clang.sh gcc
 
 printf "\n"
 printf "No errors were detected on any of the compilers.  Everything looks fine.\n"
--- scripts/build_and_test_clang.sh
+++ scripts/build_and_test_clang.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-# cleanup old files
-if [ -f ./bin/linux/debug/tests/clang/hlml_tests_c.exe ];
-then
-	rm ./bin/linux/debug/tests/clang/hlml_tests_c.exe
+# exit when any command fails
+set -e
+
+# must be either:
+# 	clang
+# 	gcc
+compiler=$1
+
+compiler_c=""
+compiler_cpp=""
+
+if [[ "${compiler}" == "clang" ]]; then
+	compiler_c=clang
+	compiler_cpp=clang++
+elif [[ "${compiler}" == "gcc" ]]; then
+	compiler_c=gcc
+	compiler_cpp=g++
+else
+	printf "ERROR: Compiler was not set.\n"
+	exit 1
 fi
 
-if [ -f ./bin/linux/debug/tests/clang/hlml_tests_cpp.exe ];
+# cleanup old files
+if [[ -f ./bin/linux/debug/tests/${compiler_c}/hlml_tests_c.exe ]];
 then
-	rm ./bin/linux/debug/tests/clang/hlml_tests_cpp.exe
+	rm ./bin/linux/debug/tests/${compiler_c}/hlml_tests_c.exe
+fi
+
+if [[ -f ./bin/linux/debug/tests/${compiler_cpp}/hlml_tests_cpp.exe ]];
+then
+	rm ./bin/linux/debug/tests/${compiler_cpp}/hlml_tests_cpp.exe
 fi
 
 
@@ -22,17 +44,17 @@ printf "\n"
 
 # C99 tests
 printf "Compiling C99 tests...\n"
-source ./scripts/build_clang.sh --output hlml_tests_c.exe --config debug --source code/generated_files/tests/c/test_main.c
+source ./scripts/build_${compiler_c}.sh --output hlml_tests_c.exe --config debug --source code/generated_files/tests/c/test_main.c
 printf "\n"
 printf "Running C99 tests...\n"
-./bin/linux/debug/tests/clang/hlml_tests_c.exe $@
+./bin/linux/debug/tests/${compiler_c}/hlml_tests_c.exe $@
 printf "\n"
 
 
 # C++ tests
 printf "Compiling C++ tests...\n"
-source ./scripts/build_clang++.sh --output hlml_tests_cpp.exe --config debug --source code/generated_files/tests/cpp/test_main.cpp
+source ./scripts/build_${compiler_cpp}.sh --output hlml_tests_cpp.exe --config debug --source code/generated_files/tests/cpp/test_main.cpp
 printf "\n"
 printf "Running C++ tests...\n"
-./bin/linux/debug/tests/clang++/hlml_tests_cpp.exe $@
+./bin/linux/debug/tests/${compiler_cpp}/hlml_tests_cpp.exe $@
 printf "\n"
